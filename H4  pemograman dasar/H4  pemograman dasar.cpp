9c8e3af9ad41072ed79c839f78e167b55d957fc1
Fathir <Fathir@MSI>
2024-05-07 17:51:30 +0700

    Add project files.

---
+++ H4  pemograman dasar/H4  pemograman dasar.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+
+class mahasiswa {
+public:
+	int nim;
+	void showNim() {
+		cout<<"No induk = "<<nim<<endl;
+
+	}
+};
+
+int main(
+
+	mahasiswa mhs{ 1 }; //p=
+	mhs.showNim(); //member access operator
+
+	mahasiswa& showNim() //object mhs
+	refMhs.nim = 2; //member acess operator
+	mhs.showNim();
+	return 0;
+
+
